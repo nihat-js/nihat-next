332faaadd088fa6bd1cfea318a35e7ec41544614
Nihat <abdullazadenihat@gmail.com>
2024-11-12 00:27:27 +0400

    test

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
                 <div className="col-span-7">
                     <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold"> Hi, I am developer</h1>
                     <p className="text-[#ADB7BE]  text-lg mb-6 lg:text-xl" >
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, deleniti?
+                        Lorem ipsum dolor sit amett consectetur adipisicing elit. Quis, deleniti?
                     </p>
                     <div>
                         <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black"> Hire me </button>
