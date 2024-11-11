4d6df1157d0711b45d0507af22bd571aa5738e86
Nihat <abdullazadenihat@gmail.com>
2024-11-11 20:43:55 +0400

    Add initial configuration

---
+++ app/haqqimda/page.tsx
@@ -0,0 +1,7 @@
+export default function HaqqimdaPage() {
+    return <main className="flex min-h-screen flex-col bg-[#121212] ">
+        <h1 className="text-white"> Portfolio Website  </h1>
+
+    </main>
+
+}
